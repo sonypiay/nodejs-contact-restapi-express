633ce26fdfbf191dd8b06d4c5e39ff1d8a2b90fe
sonypiay <pyscho30@gmail.com>
2024-03-07 00:14:10 +0700

    adjust validation: - add abort early to false

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,7 +1,9 @@
 import {ResponseError} from "../errors/response-error.js"
 
 const validate = (schema, request) => {
-    const result = schema.validate(request);
+    const result = schema.validate(request, {
+        abortEarly: false
+    });
 
     if( result.error ) {
         throw new ResponseError(400, result.error.message);
