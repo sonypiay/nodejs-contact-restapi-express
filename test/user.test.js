8333f1014ac5bab716459fb1e6945ecf39e7ad3c
sonypiay <pyscho30@gmail.com>
2024-03-07 00:14:23 +0700

    adjust unit test: - user test

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,7 @@
 import { web } from "../src/application/web.js";
 import { prismaClient } from "../src/application/database.js";
 import supertest from "supertest";
+import { logger } from "../src/application/logging.js";
 
 describe('POST /api/users/register', function() {
 
@@ -25,5 +26,50 @@ describe('POST /api/users/register', function() {
         expect(result.body.data.username).toBe('sonypiay');
         expect(result.body.data.name).toBe('Sony Darmawan');
         expect(result.body.data.password).toBeUndefined();
+    });
+
+    it('should reject if request is invalid', async () => {
+        const result = await supertest(web)
+        .post('/api/users/register')
+        .send({
+            username: '',
+            password: '',
+            name: ''
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
+    });
+
+    it('should reject if user exists', async () => {
+        let result = await supertest(web)
+        .post('/api/users/register')
+        .send({
+            username: 'sonypiay',
+            password: 'rahasia',
+            name: 'Sony Darmawan'
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe('sonypiay');
+        expect(result.body.data.name).toBe('Sony Darmawan');
+        expect(result.body.data.password).toBeUndefined();
+
+        result = await supertest(web)
+        .post('/api/users/register')
+        .send({
+            username: 'sonypiay',
+            password: 'rahasia',
+            name: 'Sony Darmawan'
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
     });
 });
